7c53fa4e1ce20043dff71e06411630fba4c86d0c
William Animate <75321465+WilliamAnimate@users.noreply.github.com>
2024-01-20 19:08:10 -0500

    massively reduce binary size
    394.3 KiB => 78.1 KiB

---
+++ build.sh
@@ -0,0 +1,2 @@
+RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --target x86_64-unknown-linux-gnu --profile optimized-build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
+
