bcf8f50a86136dc8e2699fb36b185ec079178a88
William Animate <75321465+WilliamAnimate@users.noreply.github.com>
2024-01-22 01:03:11 -0500

    add message saying to stop removing build.sh
    looking at you, xxandr

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# stop removing this file; run this to quickly build in release mode
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --target x86_64-unknown-linux-gnu --profile optimized-build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
