ef8123486c877d08c9b3604ad86cd385d6c85086
xxandr <111686716+XandrCopyrighted@users.noreply.github.com>
2024-01-23 22:38:38 -0500

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-# stop removing this file; run this to quickly build in release mode
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --target x86_64-unknown-linux-gnu --profile optimized-build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
