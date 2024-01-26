f347c3c5319ee2e6ebe8f97dead2a0c5f7757608
William Animate <75321465+WilliamAnimate@users.noreply.github.com>
2024-01-26 12:04:34 -0500

    remove nixos support
    maybe some other day

--- src/packages.rs
+++ src/packages.rs
@@ -37,30 +37,3 @@ pub fn get_num_packages() -> i16 {
     }
 }
 
-pub fn packages_nixos_based() -> Result<String, String> {
-    // Use `nix-store -qR /run/current-system/sw/ 2>/dev/null && nix-store -qR ~/.nix-profile/`
-    // to get the list of installed packages. So, we will first get the output for the
-    // first command in the shell chain.
-    let packages = Command::new("nix-store")
-        .args(["-qR", "/run/current-system/sw/"])
-        .output();
-
-    match packages {
-        Ok(x) => {
-            // Once the first chain command succeeds, we will add it's output,
-            // to the second chain command.
-            let packages_output = String::from_utf8(x.stdout).unwrap();
-            match Command::new("nix-store")
-                .args(["-qR", "~/.nix-profile/"])
-                .output()
-            {
-                Ok(y) => {
-                    let prev_output = String::from_utf8(y.stdout).unwrap();
-                    Ok(format!("{}{}", packages_output, prev_output))
-                }
-                Err(e) => Err(e.to_string()),
-            }
-        }
-        Err(e) => Err(e.to_string()),
-    }
-}
