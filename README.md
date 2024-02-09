c4cc27651e7a487c117ac5d6aa8791245151f4b4
Xander LMAO <xxandrr@proton.me>
2024-02-09 01:57:14 +0000

    archlinux only

--- README.md
+++ README.md
@@ -5,12 +5,7 @@ A simple fetch written in Rust.
 
 ---
 
-`xFetch` only supports these package managers:
-*  pacman
-*  yum
-*  apt
-
-## Install
+Note: `xFetch` only supports the *[pacman](https://wiki.archlinux.org/title/pacman)* package manager.
 * [Arch User Repository](https://aur.archlinux.org/packages/xfetch-bin)
 * [GitHub Releases](https://github.com/XandrCopyrighted/xFetch/tags)
 
@@ -18,7 +13,7 @@ A simple fetch written in Rust.
 ```sh
 git clone https://github.com/XandrCopyrighted/xFetch.git
 cd xFetch
-rustup override set nightly-2024-01-20
-rustup component add rust-src --toolchain nightly-2024-01-20-x86_64-unknown-linux-gnu
+rustup override set nightly
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
 RUSTFLAGS="-Zlocation-detail=none" cargo build --target x86_64-unknown-linux-gnu --profile optimized-build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
 ```
--- src/packages.rs
+++ src/packages.rs
@@ -2,51 +2,9 @@
 use std::{fs, path::Path};
 
 const PACMAN_DIR: &str = "/var/lib/pacman/local";
-const DNF_DIR: &str = "/var/cache/dnf";
-const APT_DIR: &str = "/var/cache/apt/archives";
-
-macro_rules! count_files_in_directory {
-    ($path:expr) => {
-        fs::read_dir($path)
-            .map(|dir| dir.count() as i16)
-            .ok()
-    }
-}
-
-macro_rules! file_exists_in_bin {
-    ($file_to_search:expr) => {
-        Path::new(&format!("/bin/{}", $file_to_search)).exists()
-    }
-}
-
-pub enum PackageManager {
-    Pacman,
-    Dnf,
-    Apt,
-    Unknown,
-}
 
 pub fn get_num_packages() -> Option<i16> {
-    let package_manager = detect_package_manager();
-    match package_manager {
-        PackageManager::Pacman => count_files_in_directory!(PACMAN_DIR),
-        PackageManager::Dnf => count_files_in_directory!(DNF_DIR),
-        PackageManager::Apt => count_files_in_directory!(APT_DIR),
-        PackageManager::Unknown => {
-            todo!("handle errors here, instead of returning None.\nBECAUSE THEN IT WILL CRASH.");
-            // None
-        }
-    }
-}
-
-fn detect_package_manager() -> PackageManager {
-    if file_exists_in_bin!("pacman") {
-        PackageManager::Pacman
-    } else if file_exists_in_bin!("dnf") {
-        PackageManager::Dnf
-    } else if file_exists_in_bin!("apt") {
-        PackageManager::Apt
-    } else {
-        PackageManager::Unknown
-    }
+    fs::read_dir(PACMAN_DIR)
+        .map(|dir| dir.count() as i16)
+        .ok()
 }
