13f765b6cb1455b28b7d954605c09bf1630c93e1
Xander LMAO <xxandrr@proton.me>
2024-02-09 02:49:19 +0000

    fixed bottleneck no. 1

--- src/packages.rs
+++ src/packages.rs
@@ -1,10 +1,9 @@
 // Made by William Animate [https://github.com/WilliamAnimate]
 use std::{fs, path::Path};
+use std::fs::read_dir;
 
 const PACMAN_DIR: &str = "/var/lib/pacman/local";
 
 pub fn get_num_packages() -> Option<i16> {
-    fs::read_dir(PACMAN_DIR)
-        .map(|dir| dir.count() as i16)
-        .ok()
+    Some(read_dir(Path::new(PACMAN_DIR)).unwrap().count() as i16)
 }
